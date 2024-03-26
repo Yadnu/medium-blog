5d7998b69a308940f091e60421bbaf55b70f87bb
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-03-26 12:53:32 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,22 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+
+app.post("/api/v1/signup", (c)=>{
+  return c.text("From signup")
+});
+app.post("/api/v1/signin", (c)=>{
+  return c.text("From signin")
+});
+app.post("/api/v1/blog", (c)=>{
+  return c.text("From blog")
+});
+app.put("/api/v1/blog", (c)=>{
+  return c.text("Edit blog")
+});
+app.get("/api/v1/blog:id", (c)=>{
+  return c.text("From blogs")
+});
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
