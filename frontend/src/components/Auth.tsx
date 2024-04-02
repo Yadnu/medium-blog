4534a166700b33e4af1c4bd730f1052b9b9ff3f9
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-04-02 11:43:45 +0530

    Completed signin and signup component

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,17 +1,53 @@
-import { ChangeEvent } from "react";
-import { Link } from "react-router-dom"
-export const Auth = ({type}:{type:"signup" | "signin"}) => {
-    return <div className=" h-screen flex justify-center flex-col">
-            <div className="flex justify-center">
-            <div className="text-3xl font-extrabold ">Create an account
-            </div> 
-            <div className="text-slate-400"> 
-                Already have an account? 
-                <Link className="pl-2 underline" to={"/signin"}>Login</Link> 
-            </div>
-            </div>
-           
+import { ChangeEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { SignupInput } from "@yadneya/common-medium";
 
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
+    const [postInputs, setPostInputs] = useState<SignupInput>({
+        name: "",
+        username: "",
+        password: ""
+    });
+
+    
+    return <div className="h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div>
+                <div className="px-10">
+                    <div className="text-3xl font-extrabold">
+                        Create an account
+                    </div>
+                    <div className="text-slate-500">
+                        {type === "signin" ? "Don't have an account?" : "Already have an account?" }
+                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                            {type === "signin" ? "Sign up" : "Sign in"}
+                        </Link>
+                    </div>
+                </div>
+                <div className="pt-8">
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Yadneya Joshi..." onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            name: e.target.value
+                        })
+                    }} /> : null}
+                    <LabelledInput label="Username" placeholder="yadneya@gmail.com" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            username: e.target.value
+                        })
+                    }} />
+                    <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            password: e.target.value
+                        })
+                    }} />
+                    <button onClick={()=>{}} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
+                </div>
+            </div>
+        </div>
     </div>
 }
 
